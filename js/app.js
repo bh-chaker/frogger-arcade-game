4040c16b1a38bad8858bc512dbedb0c4fc9d7e01
Chaker Ben Hammouda <benhammouda.chaker@gmail.com>
2015-02-12 16:28:25 +0100

    Pushing version 1.0

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Enemy, Player, Gem and Key classes will use this class
+var Displayable = function (sprite, x, y, displayed){
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+    this.displayed = displayed;
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+Displayable.prototype.render = function(){
+    if (this.displayed){
+        ctx.drawImage(Resources.get(this.sprite), this.x*offsetX, this.y*offsetY-25);
+    }
+};
+
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    // Enemy constructor will take the initial position (x,y) and add
+    // Then, it will call Displayable construtor with 'enemy-bug' image path.
+    Displayable.call(this, 'images/enemy-bug.png', x, y, true);
 }
+// Enherit the render function from Displayable
+Enemy.prototype = Object.create(Displayable.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,27 +28,150 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > numCols-1){
+        this.x = getRandomInt(-3, 0);
+    }
+    else{
+        this.x+= dt;
+    }
 }
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+
+    Displayable.call(this, 'images/char-boy.png', startX, startY, true);
+
+    this.lives = 4;
+    this.gems = 0;
+    this.dead = false;
+    this.deadSince = Date.now();
+}
+Player.prototype = Object.create(Displayable.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function (dt) {
+    var length = allGems.length;
+    if (this.dead){
+        if (Date.now()-this.deadSince > 50000*dt){
+            this.dead = false;
+            this.x = startX;
+            this.y = startY;
+            if (this.lives>0){
+                this.lives--;
+            }
+        }
+    }
+    else{
+        for (var i=length-1; i>=0; i--){
+            var gem = allGems[i];
+            if (player.y==gem.y && player.x==gem.x){
+                allGems.splice(i, 1);
+                this.gems++;
+            }
+        }
+        var length = allEnemies.length;
+        for (var i=0; i<length; i++){
+            var enemy = allEnemies[i];
+            if (player.y==enemy.y && player.x - enemy.x < 0.7 && enemy.x - player.x < 0.7){
+                this.dead = true;
+                this.deadSince = Date.now();
+            }
+        }
+    }
+
+}
+
+Player.prototype.render = function (topMargin) {
+    var offset = 0;
+
+    if (this.dead){
+        offset = 20;
+    }
+    var img = Resources.get(this.sprite);
+    ctx.drawImage(img, this.x*offsetX-offset/2, topMargin+this.y*offsetY-25, img.width+offset, img.height+offset);
+}
+
+Player.prototype.handleInput = function(input){
+    if (this.dead){
+        return;
+    }
+    if (input=='up' && this.y>0){
+        this.y--;
+    }
+
+    if (input=='down' && this.y<startY){
+        this.y++;
+    }
+
+    if (input=='left' && this.x>0){
+        this.x--;
+    }
+
+    if (input=='right' && this.x<numCols-1){
+        this.x++;
+    }
+}
+
+var getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
 
 
+function init_enemies(){
+    allEnemies = [];
+    for (var i=0; i<3; i++){
+        var enemy = new Enemy(getRandomInt(0,numCols-1), i+1);
+        allEnemies.push(enemy);
+    }
+}
+
+var allGems = [];
+var gemSprites = [
+    'images/Gem Blue.png',
+    'images/Gem Green.png',
+    'images/Gem Orange.png',
+];
+
+function init_gems(){
+    allGems = [];
+    for (var i=0; i<3; i++){
+        var gem = new Displayable(gemSprites[getRandomInt(0,3)], getRandomInt(0, numCols-1), i+1, true);
+        allGems.push(gem);
+    }
+}
+
+var player;
+function init_player(){
+    player = new Player();
+}
+
+var key;
+function init_key(){
+    key = new Displayable('images/Key.png', startX, 0, false);
+}
+
+function init_all(){
+    init_enemies();
+    init_gems();
+    init_player();
+    init_key();
+    document.addEventListener('keyup', playerKeyupListener);
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+var playerKeyupListener = function (e){
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -43,4 +180,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+};
+
--- js/engine.js
+++ js/engine.js
@@ -23,10 +23,34 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
+        lastTime,
+        starTime,
+        numRows = 6,
+        numCols = 8,
+        tileWidth = 101,
+        tileHeight = 101,
+        offsetX = 101,
+        offsetY = 83;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    var gameOver = false;
+    var gameOverTime;
+    var gameOverDisplayed = false;
+
+    var topMargin = 0;
+
+    var level;
+
+    var rowTiles = [
+        'grass',
+        'stone',
+        'stone',
+        'stone',
+        'grass',
+        'grass'
+    ];
+
+    canvas.width = numCols * tileWidth;
+    canvas.height = (numRows+1) * tileHeight;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -45,8 +69,18 @@ var Engine = (function(global) {
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        update(dt);
-        render();
+         if (gameOver){
+            if (!gameOverDisplayed){
+                displayGameOverScreen();
+                gameOverDisplayed = true;
+                doc.addEventListener('keyup', gameOverKeyupListener);
+            }
+         }
+         else{
+            update(dt);
+            render();
+         }
+
 
         /* Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
@@ -65,7 +99,17 @@ var Engine = (function(global) {
      */
     function init() {
         reset();
+        startTime = Date.now();
         lastTime = Date.now();
+        level = {
+            'currentLevel': 1,
+            'loadingNextLevel': false,
+            'currentRow': 0
+        };
+
+        gameOver = false;
+        gameOverDisplayed = false;
+
         main();
     }
 
@@ -91,10 +135,42 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        player.update();
+        if (level.loadingNextLevel){
+            if (topMargin < offsetY){
+                topMargin += 150 * dt;
+            }
+            else{
+                topMargin -= offsetY;
+                level.currentRow += 1;
+                if (level.currentRow < numRows){
+                    rowTiles.unshift(rowTiles.pop());
+                }
+                else{
+                    init_enemies();
+                    init_gems();
+                    topMargin = 0;
+                    rowTiles.unshift(rowTiles.pop());
+                    level.loadingNextLevel = false;
+                    player.y = numRows - 2;
+                    doc.addEventListener('keyup', playerKeyupListener);
+                }
+            }
+        }
+        else{
+            allEnemies.forEach(function(enemy) {
+                if (!player.dead){
+                    enemy.update(dt + dt*level.currentLevel*0.5);
+                }
+            });
+            player.update(dt);
+            if (player.lives==0){
+                doc.removeEventListener('keyup', playerKeyupListener);
+                gameOver = true;
+                gameOverTime = Date.now();
+                gameOverDisplayed = false;
+            }
+        }
+
     }
 
     /* This function initially draws the "game level", it will then call
@@ -107,17 +183,16 @@ var Engine = (function(global) {
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
-        var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
-            ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
+        var row, col;
+
+
+        // Disploy bottom blocks at the top to give an impression of infinite length
+        for (col = 0; col < numCols; col++) {
+            ctx.drawImage(Resources.get('images/'+ rowTiles[numRows-2] +'-block.png'),
+                col * tileWidth, topMargin - 2*offsetY);
+            ctx.drawImage(Resources.get('images/'+ rowTiles[numRows-1] +'-block.png'),
+                col * tileWidth, topMargin - offsetY);
+        }
 
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
@@ -132,12 +207,21 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                var imgPath = 'images/'+ rowTiles[row] +'-block.png';
+                ctx.drawImage(Resources.get(imgPath), col * tileWidth, topMargin + row * offsetY);
             }
         }
 
+        displayFooter();
+        displayInstructions();
 
         renderEntities();
+
+        if (level.loadingNextLevel){
+            displayLoadingNextLevel();
+
+        }
+
     }
 
     /* This function is called by the render function and is called on each game
@@ -152,7 +236,170 @@ var Engine = (function(global) {
             enemy.render();
         });
 
-        player.render();
+        allGems.forEach(function(gem) {
+            gem.render();
+        });
+
+        key.render();
+
+        if (key.displayed){
+            if (key.x==player.x && key.y==player.y){
+                key.displayed = false;
+                console.log('key is hidden');
+                allEnemies = [];
+                level.currentLevel++;
+                level.loadingNextLevel = true;
+                level.currentRow = 0;
+                topMargin = 0;
+                doc.removeEventListener('keyup', playerKeyupListener);
+            }
+            else{
+                player.render(topMargin);
+            }
+        }
+        else if (!level.loadingNextLevel && allGems.length==0){
+            key.displayed = true;
+        }
+        else{
+            if (!level.loadingNextLevel){
+                player.render(topMargin);
+            }
+            else{
+                if (level.currentRow < numRows-3){
+                    player.y = level.currentRow;
+                }
+                else{
+                    player.y = numRows-3;
+                }
+
+                player.render(topMargin);
+            }
+        }
+    }
+
+    function displayGameOverScreen(){
+        ctx.fillStyle = "#F67841";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "#67200A";
+        ctx.textAlign = 'center';
+        ctx.font="50px Georgia";
+
+        ctx.fillText("Game Over", canvas.width/2, 1*tileHeight);
+        ctx.fillText("You reached level "+level.currentLevel, canvas.width/2, 2*tileHeight);
+        ctx.fillText("You collected "+player.gems+" gems", canvas.width/2, 3*tileHeight);
+        var playedTime = gameOverTime - startTime;
+        playedTime = msToTime(playedTime);
+        ctx.fillText("Total time "+playedTime, canvas.width/2, 4*tileHeight);
+        ctx.fillText("Press any key to restart", canvas.width/2, 5*tileHeight);
+    }
+
+    function gameOverKeyupListener(e){
+        gameOver = false;
+        doc.removeEventListener('keyup', gameOverKeyupListener);
+        init();
+    }
+
+    function displayFooter(){
+        displayFooterBoxes();
+        displayFooterLivesCount();
+        displayFooterGemsCont();
+        displayFooterLevel();
+        displayFooterTime();
+    }
+
+    function displayFooterBoxes(){
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "green";
+        ctx.textAlign = "left";
+        ctx.lineWidth = 4;
+        ctx.fillRect(0, canvas.height-231, canvas.width, 130);
+        ctx.strokeRect(0*2*tileWidth+ctx.lineWidth/2, canvas.height-231, 2*tileWidth-ctx.lineWidth, 130);
+        ctx.strokeRect(1*2*tileWidth+ctx.lineWidth/2, canvas.height-231, 2*tileWidth-ctx.lineWidth, 130);
+        ctx.strokeRect(2*2*tileWidth+ctx.lineWidth/2, canvas.height-231, 4*tileWidth-ctx.lineWidth, 65);
+        ctx.strokeRect(2*2*tileWidth+ctx.lineWidth/2, canvas.height-166, 4*tileWidth-ctx.lineWidth, 65);
+    }
+
+    function displayFooterLivesCount(){
+        ctx.drawImage(Resources.get('images/Heart.png'), 0, (numRows-1) * offsetY + 35);
+        ctx.font="50px Georgia";
+        ctx.fillStyle = "#F67841";
+        ctx.strokeStyle = "#67200A";
+        ctx.lineWidth = 2;
+        ctx.fillText("x "+player.lives,111, numRows * offsetY + 50);
+        ctx.strokeText("x "+player.lives,111, numRows * offsetY + 50);
+    }
+
+    function displayFooterGemsCont(){
+        ctx.drawImage(Resources.get('images/Gem Blue.png'), offsetX*2+6, (numRows-1) * offsetY + 15);
+        ctx.font="50px Georgia";
+        ctx.fillStyle = "#F67841";
+        ctx.strokeStyle = "#67200A";
+        ctx.lineWidth = 2;
+        ctx.fillText("x "+player.gems,offsetX*3+10, numRows * offsetY + 50);
+        ctx.strokeText("x "+player.gems,offsetX*3+10, numRows * offsetY + 50);
+    }
+
+    function displayFooterLevel(){
+        ctx.font="50px Georgia";
+        ctx.fillStyle = "#F67841";
+        ctx.strokeStyle = "#67200A";
+        ctx.lineWidth = 2;
+        ctx.fillText("Level",offsetX*4+10, numRows * offsetY + 30);
+        ctx.strokeText("Level",offsetX*4+10, numRows * offsetY + 30);
+
+        ctx.textAlign = "right";
+        ctx.fillText(level.currentLevel,offsetX*6, numRows * offsetY + 30);
+        ctx.strokeText(level.currentLevel,offsetX*6, numRows * offsetY + 30);
+    }
+
+    function displayFooterTime(){
+        ctx.textAlign = "left";
+        ctx.font="50px Georgia";
+        ctx.fillStyle = "#F67841";
+        ctx.strokeStyle = "#67200A";
+        ctx.lineWidth = 2;
+
+        var playedTime = Date.now() - startTime;
+        playedTime = msToTime(playedTime);
+        ctx.fillText("Time",offsetX*4+10, numRows * offsetY + 85);
+        ctx.strokeText("Time",offsetX*4+10, numRows * offsetY + 85);
+        ctx.textAlign = "right";
+        ctx.fillText(playedTime, canvas.width-20, numRows * offsetY + 85);
+        ctx.strokeText(playedTime, canvas.width-20, numRows * offsetY + 85);
+    }
+
+    function displayInstructions(){
+        ctx.fillStyle = "#F67841";
+        ctx.fillRect(0, 6*tileWidth + 2, canvas.width, tileWidth);
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "#67200A";
+        ctx.textAlign = 'left';
+        ctx.font="30px Georgia";
+        ctx.fillText("1) Use arrows to move.", 10, 6.3*tileHeight);
+        ctx.fillText("2) Collect all 3 gems to make the key appear.", 10, 6.6*tileHeight);
+        ctx.fillText("3) Collect the key to advance to next level.", 10, 6.9*tileHeight);
+    }
+
+    function displayLoadingNextLevel(){
+        ctx.textAlign = "center";
+        ctx.font="80px Georgia";
+        var color1 = "#F67841";
+        var color2 = "#67200A";
+        var t = Date.now();
+        if ((t-t%500)%1000==0){
+            ctx.fillStyle = color1;
+            ctx.strokeStyle = color2;
+        }
+        else{
+            ctx.fillStyle = color2;
+            ctx.strokeStyle = color1;
+        }
+
+        ctx.lineWidth = 2;
+        ctx.fillText("Loading Next Level",canvas.width/2, 120);
+        ctx.strokeText("Loading Next Level",canvas.width/2, 120);
     }
 
     /* This function does nothing but it could have been a good place to
@@ -160,7 +407,7 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+        init_all();
     }
 
     /* Go ahead and load all of the images we know we're going to need to
@@ -168,11 +415,18 @@ var Engine = (function(global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png',
+        'images/Heart.png',
+        'images/background-block.png',
+        'images/empty-block.png',
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/Key.png'
     ]);
     Resources.onReady(init);
 
@@ -181,4 +435,29 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
+    global.numRows = numRows;
+    global.numCols = numCols;
+    global.startX = 3;
+    global.startY = 4;
+    global.tileWidth = tileWidth;
+    global.tileHeight = tileHeight;
+    global.offsetX = offsetX;
+    global.offsetY = offsetY;
 })(this);
+
+
+function msToTime(s) {
+
+  function addZ(n) {
+    return (n<10? '0':'') + n;
+  }
+
+  var ms = s % 1000;
+  s = (s - ms) / 1000;
+  var secs = s % 60;
+  s = (s - secs) / 60;
+  var mins = s % 60;
+  var hrs = (s - mins) / 60;
+
+  return addZ(hrs) + ':' + addZ(mins) + ':' + addZ(secs) + '.' + Math.floor(ms/100.0);
+}
